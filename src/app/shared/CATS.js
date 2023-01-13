f96e067b1064a9f49492f946c925632e2d933f1c
Dan Galuardi <dan.galuardi@gmail.com>
2023-01-13 13:29:43 -0500

    Image assets and available animal arrays

---
+++ src/app/shared/CATS.js
@@ -0,0 +1,55 @@
+import winston from '../assets/img/winstonCat.jpg'
+import otho from '../assets/img/othoCat.jpg'
+import charlieBrown from '../assets/img/charlieBrownCat.jpg';
+import barracuda from '../assets/img/barracudaCat.jpg';
+
+export const CATS = [
+    {
+        id: 0,
+        name: 'Winston',
+        image: winston,
+        birthday: 'June 2020',
+        sex: 'Male',
+        breed: 'DSH',
+        weight: '13 lbs',
+        arrival: '',
+        backgroung: 'Meet Winston!  He was found in tree all by himself.  He has lots of energy and enjoys the company of other cats or dogs.  His favorite activity is jumping up walls.',
+        featured: true
+    },
+    {
+        id: 1,
+        name: 'Otho',
+        image: otho,
+        birthday: 'June 2022',
+        sex: 'Male',
+        breed: 'DSH',
+        weight: '',
+        arrival: '',
+        backgroung: "Otho is a cute little boy who was found outside with his sisters. He has a cute pink nose and pink toe beans. Since being with us he has been getting used to being around people and having people pay him attention. He's starting to come around and realize that being pet can feel good! He still startles easily and will need a home on the calm side so he can feel confident at his own pace. This little cutie has a whole lifetime of affection to soak in from his new family!",
+        featured: false
+    },
+    {
+        id: 2,
+        name: 'Charlie Brown',
+        image: charlieBrown,
+        birthday: 'December 2014',
+        sex: 'Male',
+        breed: 'DSH',
+        weight: '',
+        arrival: '',
+        backgroung: `Charlie Brown is a large and lovely cat. He tips the scales at 15 pounds. But we think most of that is just a big bunch of love! He loves hanging out with people and getting pets. He'll make biscuits with his paws and may even "nurse" on his blanket when he's happy. Charlie Brown is a delightful and friendly boy who is ready to find someone to give him all the good things in life!`,
+        featured: false
+    },
+    {
+        id: 3,
+        name: 'Barracuda',
+        image: barracuda,
+        birthday: 'August 2022',
+        sex: 'Female',
+        breed: 'DSH',
+        weight: '',
+        arrival: '',
+        backgroung: `Barracuda was found outside along with her siblings. They have been in a foster home where they've been having a great time and learning all the fun things about being kittens. Barracuda especially enjoyed climbing, jumping, and chasing her toys. She loves being around other cats, and was even O.K. with the dog in the home, as long as he didn't get too much in her face! She is very shy about meeting new people and it takes some extra time and patience to get to know her. The way to her heart is through treats! Barracuda will be a happy girl when she finds her forever home and someone to give her all the love she deserves!`,
+        featured: false
+    }
+];
